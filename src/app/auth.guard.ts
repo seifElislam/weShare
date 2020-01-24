78d9d0098d453a14f7c9fc8da0ff4982d0a33eb2
seifElislam <seif.eleslam1990@gmail.com>
2020-01-25 01:41:38 +0200

    feat: Update auth gard to redirect to home if logged in user

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean|UrlTree> | boolean {
+    return this.auth.isAuthenticated$.pipe(
+      tap(loggedIn => {
+        console.log('loggedin: ', loggedIn)
+        if (!loggedIn) {
+          console.log('redirect...')
+          // this.auth.login(state.url);
+          this.router.navigate(['']);
+        }
+      })
+    );
   }
-  
+
 }
