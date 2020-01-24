187cacd82e2f07d9c2751818659872f57c53962a
seifElislam <seif.eleslam1990@gmail.com>
2020-01-25 01:40:09 +0200

    feat: Update auth0 domain & clientId

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'jfqyzrayu1'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-cksu441t.auth0.com',            // Auth0 domain
+  clientId: 'aBl23X5AEMqQe40CD4cSHrqvZg5PAUGR',          // Auth0 client id
+  callbackUrl: 'http://localhost:4200/home'
+}
